2458d44144549c53e4802f0c83747b61f5be1d4c
Ian Harrier <github@harrier.us>
2017-05-30 10:16:51 -0500

    Clean up entrypoint scripts

--- images/confluence/docker-entrypoint.sh
+++ images/confluence/docker-entrypoint.sh
@@ -1,34 +1,22 @@
 #!/bin/sh
 set -e
 
-# This script modifies itself after successfully preforming tasks, preventing
-# the tasks from running every time the container is restarted.
-PERMISSIONS_COMPLETE=false
-PROXY_COMPLETE=false
-TIMEZONE_COMPLETE=false
+echo "[I] Setting permissions on Confluence home directory."
+chown -R ${RUN_USER}:${RUN_GROUP}  "${CONFLUENCE_HOME}"
+chmod -R u=rwx,go-rwx              "${CONFLUENCE_HOME}"
 
-if [ "$PERMISSIONS_COMPLETE" = "false" ]; then
-    echo "[I] Setting permissions on Confluence home directory."
-    chmod -R 700                       "$CONFLUENCE_HOME"
-    chown -R ${RUN_USER}:${RUN_GROUP}  "$CONFLUENCE_HOME"
-    sed -i 's/^PERMISSIONS_COMPLETE=.*/PERMISSIONS_COMPLETE=true/g' /usr/local/bin/docker-entrypoint
-fi
-
-if [ "$PROXY_COMPLETE" = "false" ]; then
-    if [ "$PROXY_HOSTNAME" -a "$PROXY_PORT" -a "$PROXY_SCHEME" ]; then
+if [ "$PROXY_HOSTNAME" -a "$PROXY_PORT" -a "$PROXY_SCHEME" ]; then
+    PROXY_STRING="proxyName=\"$PROXY_HOSTNAME\" proxyPort=\"$PROXY_PORT\" scheme=\"$PROXY_SCHEME\""
+    if [ ! "$(cat ${CONFLUENCE_INSTALL}/conf/server.xml | grep "<Connector $PROXY_STRING")" ]; then
         echo "[I] Configuring Catalina to operate behind a reverse proxy."
-        sed -i "s/\(<Connector\)/\1 proxyName=\"$PROXY_HOSTNAME\" proxyPort=\"$PROXY_PORT\" scheme=\"$PROXY_SCHEME\"/" /opt/atlassian/confluence/conf/server.xml
+        sed -i "s/\(<Connector\)/\1 $PROXY_STRING/" ${CONFLUENCE_INSTALL}/conf/server.xml
     fi
-    sed -i 's/^PROXY_COMPLETE=.*/PROXY_COMPLETE=true/g' /usr/local/bin/docker-entrypoint
 fi
 
-if [ "$TIMEZONE_COMPLETE" = "false" ]; then
-    if [ "$TIMEZONE" ]; then
-        echo "[I] Setting the time zone."
-        cp "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
-        echo "$TIMEZONE" > /etc/timezone
-    fi
-    sed -i 's/^TIMEZONE_COMPLETE=.*/TIMEZONE_COMPLETE=true/g' /usr/local/bin/docker-entrypoint
+if [ "$TIMEZONE" ]; then
+    echo "[I] Setting the time zone."
+    cp "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
+    echo "$TIMEZONE" > /etc/timezone
 fi
 
 echo "[I] Entrypoint tasks complete. Starting Confluence."
